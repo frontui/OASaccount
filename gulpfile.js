3b2ad7dae6afdb2741fbb9838731f03aa2ddab36
tomieric <tomieric@gmail.com>
2016-08-31 17:50:52 +0800

    初始化

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+/**
+ *! frontman 手脚架
+ * (c) frontui team
+ */
+
+var config = require('./config.json')
+var pkg    = require('./package.json')
+var gulp   = require('gulp')
+var path   = require('path')
+var fs     = require('fs')
+var $      = require('gulp-load-plugins')()
+var Lib = require('./lib')
+
+// 服务启动根目录
+var serverRoot = __dirname
+
+/*-------------
+*  默认任务
+-------------*/
+require('./task/defaultTask')(serverRoot);
+
+/*--------
+*  更新 UI库 任务
+------------- */
+require('./task/updateTask')()
+
+/*--------
+*  合并 sprite 任务
+------------- */
+// preload: npm install gulp.spritesmith imagemin-pngquant merge-stream --save
+// require('./task/spriteTask')()
+
+
+/*-------------
+*  svn任务
+-------------*/
+// require('./task/svnTask')(Lib.banner);
+
+/*--------
+*  tfs任务
+------------- */
+// preload: npm install gulp-tfs-fp --save-dev
+// require('./task/tfsTask')()
+
+/*--------
+*  doc说明文档
+------------- */
+// require('./task/docTask')(config.docs)
